063cc5489ac3357968f37ff46ac31706cc260b49
thinhtle <thinht.le@spiraledge.com>
2025-02-09 18:25:18 +0700

    Initial Commit

---
+++ Test_FrameWork/Test_FrameWork.h
@@ -0,0 +1,18 @@
+//
+//  Test_FrameWork.h
+//  Test_FrameWork
+//
+//  Created by thinhtle on 2/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test_FrameWork.
+FOUNDATION_EXPORT double Test_FrameWorkVersionNumber;
+
+//! Project version string for Test_FrameWork.
+FOUNDATION_EXPORT const unsigned char Test_FrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test_FrameWork/PublicHeader.h>
+
+
