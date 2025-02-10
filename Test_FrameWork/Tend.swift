4de29a4a121eb7d9aeed73f93f8b48a49d123289
thinhtle <thinht.le@spiraledge.com>
2025-02-10 18:21:10 +0700

    update Véion

---
+++ Test_FrameWork/Tend.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+@objc public class Tend: NSObject {
+    
+    @objc public let version = 6
+    
+    @objc public func createCustomSubview(withText text: String) -> UIView {
+        // Tạo view con
+        let subview = UIView()
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        subview.backgroundColor = .yellow
+        subview.layer.cornerRadius = 12
+        subview.layer.borderWidth = 2
+        subview.layer.borderColor = UIColor.black.cgColor
+        
+        // Tạo label bên trong view con
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        // Thêm label vào view con
+        subview.addSubview(label)
+        
+        // Constraints cho label để căn giữa
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: subview.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: subview.centerYAnchor)
+        ])
+        
+        return subview
+    }
+}
