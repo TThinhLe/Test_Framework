760ab7eb16223051da09e968a9f71ee8aba1a29a
thinhtle <thinht.le@spiraledge.com>
2025-02-11 23:50:14 +0700

    “updated”

--- Test_FrameWork/Tend.swift
+++ Test_FrameWork/Tend.swift
@@ -2,7 +2,7 @@ import UIKit
 
 @objc public class Tend: NSObject {
     
-    @objc public let version = 38
+    @objc public let version = 39
     
     @objc public func createCustomSubview(withText text: String) -> UIView {
         print(b())
@@ -34,6 +34,6 @@ import UIKit
         return subview
     }
     @objc public func b()->String{
-        return "d"
+        return "e"
     }
 }
