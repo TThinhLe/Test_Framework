61b2715323d63fd8cc7c196d68e62b71d99965c8
thinhtle <thinht.le@spiraledge.com>
2025-02-11 08:32:30 +0700

    “updated”

--- Test_FrameWork/Tend.swift
+++ Test_FrameWork/Tend.swift
@@ -2,13 +2,13 @@ import UIKit
 
 @objc public class Tend: NSObject {
     
-    @objc public let version = 30
+    @objc public let version = 31
     
     @objc public func createCustomSubview(withText text: String) -> UIView {
         // Tạo view con
         let subview = UIView()
         subview.translatesAutoresizingMaskIntoConstraints = false
-        subview.backgroundColor = .systemRed
+        subview.backgroundColor = .systemGreen
         subview.layer.cornerRadius = 12
         subview.layer.borderWidth = 2
         subview.layer.borderColor = UIColor.black.cgColor
